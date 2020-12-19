3ccae9ae308c6883c1b997eba4192a39164ccd90
pepermz <74684215+pepermz@users.noreply.github.com>
2020-12-18 23:28:13 -0600

    shop function

--- games.js
+++ games.js
@@ -90,6 +90,12 @@ var startGame = function(){
         enemyHealth = 50;
         fight(pickedEnemyName);
         }
+        if (playerHealth> 0 && i< enemyNames.length -1){
+            var storeConfirm = window.confirm("The fight is over, visit the store before the next round?");
+            if (storeConfirm){
+            shop();
+            }
+        }
         else{
             window.alert(" You have lost your robot in battle! Game Over! ");
             break;
@@ -103,7 +109,7 @@ var startGame = function(){
             window.alert("You've lost your robot in battle.");
         }
         var playAgainConfirm = window.confirm("Would you like to play again?")
-        
+
         if (playAgainConfirm){
             startGame();
         }
@@ -112,5 +118,47 @@ var startGame = function(){
         }
     }
     endGame();
+};
+var shop = function(){
+    var shopOptionPrompt=window.prompt(
+        " Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE', or 'LEAVE' to make a choice."
+    );
+    switch(shopOptionPrompt){
+        case "REFILL": 
+        case "refill":
+            if (playerMoney >=7){
+            window.alert(" Refilling player's health by 20 for 7 dollars.");
+
+            playerHealth=playerHealth+20;
+            playerMoney=playerMoney-7;
+            }
+            else{
+                window.alert("You don't have enough money!");
+            }
+            break;
+            case "upgrade":
+            case "UPGRADE":
+                if (playerMoney >=7){
+                window.alert("Upgrading player's attack by 6 for 7 dollars.");
+            
+                playerAttack = playerAttack + 6;
+                playerMoney = playerMoney - 7;
+                }
+                else{
+                    window.alert("You don't have enough money!");
+                }
+                break;
+            case "leave":
+            case "LEAVE":
+                window.alert(" Leaving the store.");
+
+                break;
+            default:
+                window.alert(" You did not pick a valid option. Try again.");
+
+                shop();
+                break;
+        
+    }
 };
 startGame();    
